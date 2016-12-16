fbda4ef8a869f996917f5f5cb3f7fa34b6e0a0ca
singanamalarajesh <singanamala.rajesh@gmail.com>
2016-12-16 07:54:23 +0530

    testngxml

--- moodle/src/test/java/moodle/test/scripts/TestLoginModule.java
+++ moodle/src/test/java/moodle/test/scripts/TestLoginModule.java
@@ -2,11 +2,12 @@ package moodle.test.scripts;
 
 import java.util.concurrent.TimeUnit;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-import junit.framework.Assert;
 import moodle.test.commons.pages.Dashboardpage;
 import moodle.test.commons.pages.Homepage;
 import moodle.test.commons.pages.Loginpage;
@@ -14,6 +15,7 @@ import moodle.test.commons.pages.Loginpage;
 public class TestLoginModule 
 {
 	public WebDriver driver;
+	static Logger log=LogManager.getLogger(TestLoginModule.class);
 	
 	@Test
 	public void verifyLogin()
@@ -23,17 +25,19 @@ public class TestLoginModule
 		driver.get("http://localhost:81/moodle");
 		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
 		driver.manage().window().maximize();
+		log.info("chrome browser triggered");
 		Homepage home=new Homepage();
 		home.driver=this.driver;
 		home.login();
+		log.info("login triggered");
 		Loginpage login=new Loginpage();
 		login.driver=this.driver;
 		login.login("rajesh","pandu552");
-		Dashboardpage dashboard=null;
-		dashboard.driver=this.dri//ver;
-		Assert.fail();
+		log.info("login page triggered");
+		Dashboardpage dashboard=new Dashboardpage();
+		dashboard.driver=this.driver;		
 		dashboard.logout();
-		//driver.close();
+		driver.close();
 		
 	}
 	
